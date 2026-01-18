d259066b5d5cffa3934567ba7dd876d8391dc242
Alphée Sordet Salin <alphee.sordet1312@gmail.com>
2026-01-18 14:33:05 +0100

    adding footer, adding javascript,a lot of minor changes

---
+++ contact.js
@@ -0,0 +1,14 @@
+
+const email = document.getElementById("email");
+const discord = document.getElementById("discord");
+
+
+email.addEventListener("click", () => {
+
+    alert("Email : alphee.sordet1312@gmail.com")
+});
+
+discord.addEventListener("click", () => {
+
+    alert("Discord : mille3sansDouze")
+});

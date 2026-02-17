bccffa6e77f7d32892ce0954622515622e7604bb
alphee sordet salin <alphee.sordet1312@gmail.com>
2026-02-17 15:14:44 +0100

    adding discord bot projet, minor other changes

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 
 const email = document.getElementById("email");
 const discord = document.getElementById("discord");
-
+const triscwarn = document.getElementById("triscplay");
 
 email.addEventListener("click", () => {
 
@@ -11,4 +11,9 @@ email.addEventListener("click", () => {
 discord.addEventListener("click", () => {
 
     alert("Discord : mille3sansDouze")
+});
+
+triscwarn.addEventListener("click", () => {
+
+    alert("En raison du déménagement du CESI, le site de triscplay peut être hors ligne. Le code source reste disponible sur mon github. \n(Il est possible que votre navigateur bloque la redirection vers triscplay après ce pop-up)")
 });
